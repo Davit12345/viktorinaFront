9a1d2a9943733000dcce06090501ed56ea2f9a6a
Davit Karapetyan <davit.karapetyan@cypressoft.com>
2023-11-11 23:59:33 +0400

    added rooms

--- src/app/private/online-game/online-game.page.ts
+++ src/app/private/online-game/online-game.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {io} from "socket.io-client";
+import {SocketService} from "../../services/socket.service";
 
 @Component({
   selector: 'app-online-game',
   templateUrl: './online-game.page.html',
   styleUrls: ['./online-game.page.scss'],
 })
-export class OnlineGamePage implements OnInit {
+export class OnlineGamePage   {
 
-  constructor() { }
+  // private socket: any;
+  public roomName: string='';
+  public roomList:any;
+  //
+  constructor(private socketService: SocketService,) {
+    // this.socket = io('http://localhost:3000');
 
-  ngOnInit() {
+    this.socketService.init();
+
   }
 
+
+  //
+  createRoom() {
+    console.log(this.roomName)
+    this.socketService.createRoom(this.roomName)
+  }
+
+  joinRoom() {
+    console.log(this.roomName)
+    this.socketService.joinRoom(this.roomName)
+  }
+  leaveRoom() {
+    this.socketService.leaveRoom(this.roomName)
+  }
+  ngOnInit() {
+
+    this.socketService.getRoomList().subscribe(data => {
+      console.log('rooms:', data);
+      this.roomList=data;
+      // Update UI with the received message
+    });
+
+  }
 }
--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -13,19 +13,50 @@ export class SocketService {
   }
 
   sendMessage(message: string) {
-    console.log(message)
     this.socket.emit('message', message);
-    this.socket.emit('newMessage', message);
-
-
   }
+  createRoom(roomName:any) {
+    this.socket.emit('createRoom', roomName);
+  }
+
+  joinRoom(roomName:any) {
+    this.socket.emit('joinRoom',roomName);
+  }
+  leaveRoom(roomName:any) {
+    this.socket.emit('leaveRoom',roomName);
+  }
+
 
   getMessage(): Observable<string> {
     return new Observable<string>(observer => {
       this.socket.on('onMessage', (data: string) => {
-        console.log(data)
         observer.next(data);
       });
     });
   }
+  getRoomList(): Observable<string> {
+    this.socket.emit('getRoomList', {});
+    return new Observable<string>(observer => {
+      this.socket.on('roomList', (data: string) => {
+        observer.next(data);
+      });
+    });
+  }
+
+  init(){
+    this.socket.on('roomCreated', (roomName:any) => {
+      console.log(`Room created: ${roomName}`);
+    });
+
+    this.socket.on('userJoined', (userId:any) => {
+      console.log(`User joined: ${userId}`);
+    });
+
+    this.socket.on('userLeft', (userId:any) => {
+      console.log(`User left: ${userId}`);
+    });
+
+  }
+
+
 }
